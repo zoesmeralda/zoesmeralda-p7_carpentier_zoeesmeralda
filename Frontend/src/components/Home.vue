73a9df397299a007bae30846e6cad5efec2ddfbc
Zoe esmeralda Carpentier <zoz78@hotmail.fr>
2022-03-05 21:34:34 +0100

    commit sur main

---
+++ Frontend/src/components/Home.vue
@@ -0,0 +1,16 @@
+
+<template><header class="container">
+        <div class="row justify-content-center">
+            <nav class="col-1 m-auto">
+    
+               <router-link to="/Feed">  Feed </router-link>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Home"
+}
+</script>
---
+++ Frontend/src/components/Navbar.vue
@@ -0,0 +1,17 @@
+
+<template>
+   <div>
+  <b-navbar variant="faded" type="light">
+    <b-navbar-brand href="#">
+      <img src="../assets/icong.png" class="d-inline-block align-top" alt="Logo">
+      
+    </b-navbar-brand>
+  </b-navbar>
+</div>
+</template>
+
+<script>
+export default {
+    name: "Navbar"
+}
+</script>
