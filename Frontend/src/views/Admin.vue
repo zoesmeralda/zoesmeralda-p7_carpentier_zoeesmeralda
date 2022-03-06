3c458dfa81c41f61cd6b5cd6abce1568fa22f95c
Zoe esmeralda Carpentier <zoz78@hotmail.fr>
2022-03-06 13:31:08 +0100

    commit

---
+++ Frontend/src/views/Admin.vue
@@ -0,0 +1,96 @@
+
+<template>
+    <div class="container">    
+        <main class="col-12">   
+            
+            <section class="col-12">
+                <h1 class="btn btn-dark btn-lg btn-block" >Administrateur</h1>
+                 <Home></Home>
+            </section>
+            <section id="filPrincipal" class="row">
+                <div class="col-12 col-md-4">
+                
+                    <article class="card bg-light my-3 class=center-block" style="float:none;">
+                        <div class="card-header">
+                            <div class="row justify-content-around">
+                                <div class="m-1"> Bienvenue {{ nameCurrentUser }} ! </div>
+                                <button @click="localClear">Se déconnecter </button>
+                            </div>
+                        </div>
+                        <div class="card-body text-center">
+                            <div class="dropdown text-center">
+                                <p>Membre depuis le {{ creation }}</p>
+                            </div>
+                            <div>
+                                <div v-if="isAdmin">
+                                    <router-link to='/Admin'><button v-if="isAdmin" type="button"  class=" btn btn-danger mx-auto rounded p-2">VOUS ÊTES L'ADMINISTRATEUR</button></router-link> 
+                                    <router-link to='/Admin'><button  type="button"   class=" btn btn-danger mx-auto m-3 rounded p-2 buttonsPanel"><button class=" rounded p-1 m-1 "> ACTIVÉE</button></button></router-link> 
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </article> 
+                </div>   
+                
+                <article class="col col-md-8 mt-5">
+                    
+                    <div class="row justify-content-around">
+                        <button @click="toAllComments()" class="btn btn-dark btn-lg btn-block">POSTS/COMMENTS</button>
+                        <button @click="toAllUsers()" class="btn btn-dark btn-lg btn-block" >USERS</button>
+                    </div>
+                </article>
+            </section>
+        </main>
+    </div>
+</template>
+
+
+<script>
+import Home from "../components/Home";
+import axios from "axios";
+import router from "../router";
+import "../main.css";
+
+export default {
+    name: "Admin",
+    components: {
+      Home
+    },
+    data() {
+        return {
+            isAdmin: false,
+            nameCurrentUser: "",
+            creation: ""
+        }
+    },
+    created: function() {        
+        let id          = localStorage.getItem('userId');
+        let self        = this;  
+        axios.get("http://localhost:3000/api/users/" + id, { headers: {"Authorization": "Bearer " + localStorage.getItem("token")} })
+        .then(res => {  
+            self.creation           = res.data.createdAt.slice(0,10).split("-").reverse().join(".");
+            self.isAdmin            = res.data.isAdmin;
+            self.nameCurrentUser    = res.data.userName.charAt(0).toUpperCase() + res.data.userName.slice(1);       
+        })
+        .catch((error)=> { console.log(error) 
+        });    
+    },
+    methods: {
+        localClear() {
+            localStorage.clear();
+            router.push({ path : "/" });
+        },
+        seeOnePost(m) {
+            console.log(m);
+            localStorage.setItem('MessageId', m);
+            router.replace("http://localhost:8080/api/comment")
+        },
+        toAllComments() {
+            router.replace("http://localhost:8080/api/AllComments")
+        },
+        toAllUsers() {
+            router.replace("http://localhost:8080/api/AllUsers")
+        }
+    }
+}
+</script>
